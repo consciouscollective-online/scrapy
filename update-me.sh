8401142e2b5a15eb2a7e00f6cd51978f20a4126e
benjammin <edu.consciouscollective.online@gmail.com>
2020-04-05 13:29:15 +0000

    added update via email, script

--- update-me.sh
+++ update-me.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cd ~/projects/scrapy
-echo "Stage 2 scraping progress:\n" > tmp
-tail -n 5 cache.data >> tmp
 echo "Latest words scraped:\n" >> tmp
-tail -n 5 checked.txt >> tmp
+tail -n 15 checked.txt >> tmp
 echo "\nEnd transmission." >> tmp
-mail -a "From: Robot <{$1}>" -s "Update: Dictionary Scrape" $2 < ./tmp
+mail -a "From: Robot <$1>" -s "Update: Dictionary Scrape" $2 < ./tmp
 rm tmp
